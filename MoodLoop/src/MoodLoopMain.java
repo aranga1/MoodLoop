66c7664f0901ff1f5d4c0f709b700ae45a1ac8f7
aranga1 <aranga@purdue.du>
2015-11-11 08:08:09 -0500

    added experimental looping functionality

--- MoodLoop/src/MoodLoopMain.java
+++ MoodLoop/src/MoodLoopMain.java
@@ -11,11 +11,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
 import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineEvent.Type;
 import javax.sound.sampled.LineListener;
@@ -30,13 +32,13 @@ import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-public class MoodLoopMain {
-
+public class Music {
+    
     public static void main(String[] args) {
-        new MoodLoopMain();
+        new Music();
     }
-
-    public MoodLoopMain() {
+    
+    public Music() {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -44,8 +46,8 @@ public class MoodLoopMain {
                     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
                 } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
                 }
-
-                JFrame frame = new JFrame("Testing");
+                
+                JFrame frame = new JFrame("Mood Loop V0.1");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setLayout(new BorderLayout());
                 frame.add(new TestPane());
@@ -55,26 +57,30 @@ public class MoodLoopMain {
             }
         });
     }
-
+    
     public class TestPane extends JPanel {
-
+        
         private Clip clip;
         private JTextField audioFile;
         private JButton play;
         private JButton stop;
+        private JButton loop;
         private int lastFrame;
-
+        private int ct;
+        
         public TestPane() {
             setLayout(new GridBagLayout());
-
+            
             audioFile = new JTextField(20);
             play = new JButton(">");
-            stop = new JButton("[]");
-
+            stop = new JButton("[ ]");
+            loop = new JButton("O");
+            
             JPanel controls = new JPanel();
             controls.add(play);
             controls.add(stop);
-
+            controls.add(loop);
+            
             GridBagConstraints gbc = new GridBagConstraints();
             gbc.gridx = 0;
             gbc.gridy = 0;
@@ -83,7 +89,26 @@ public class MoodLoopMain {
             add(audioFile, gbc);
             gbc.gridy++;
             add(controls, gbc);
-
+            
+            
+            loop.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    ct++;
+                    System.out.println("ct:" + ct);
+                    if (ct % 2 == 0) {
+                        clip.loop(0);
+                        System.out.println("Entered this method");
+                    }
+                    else {
+                        int here = clip.getFramePosition();
+                        clip.setLoopPoints(here, -1);
+                        clip.loop(Clip.LOOP_CONTINUOUSLY);
+                        System.out.println("Entered here!");
+                    }
+                }
+            });
+            
+            
             play.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -111,9 +136,10 @@ public class MoodLoopMain {
                             JOptionPane.showMessageDialog(TestPane.this, "Failed to load audio clip", "Error", JOptionPane.ERROR_MESSAGE);
                         }
                     } else {
-
+                        
                         if (clip.isRunning()) {
                             lastFrame = clip.getFramePosition();
+                            System.out.println(lastFrame);
                             clip.stop();
                         } else {
                             if (lastFrame < clip.getFrameLength()) {
@@ -123,32 +149,34 @@ public class MoodLoopMain {
                             }
                             clip.start();
                         }
-
+                        
                     }
                 }
             });
-
+            
+            
             stop.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     if (clip != null) {
+                        ct = 0;
                         lastFrame = 0;
                         clip.stop();
+                        clip = null;
                     }
                 }
             });
         }
-
+        
         protected void loadClip(File audioFile) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
-
+            
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-
             AudioFormat format = audioStream.getFormat();
             DataLine.Info info = new DataLine.Info(Clip.class, format);
             this.clip = (Clip) AudioSystem.getLine(info);
             this.clip.open(audioStream);
-
+            
         }
     }
-
+    
 }
