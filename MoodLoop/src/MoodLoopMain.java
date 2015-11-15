6325c46f2681ea06e56cc461cff071155641d666
aranga1 <aranga@purdue.du>
2015-11-15 05:55:52 -0500

    First basic application complete

--- MoodLoop/src/MoodLoopMain.java
+++ MoodLoop/src/MoodLoopMain.java
@@ -1,3 +1,5 @@
+import it.sauronsoftware.jave.Encoder;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -28,6 +30,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -64,9 +67,10 @@ public class Music {
         private JTextField audioFile;
         private JButton play;
         private JButton stop;
-        private JButton loop;
+        private JButton loop, startHere, endHere, loopIt, reset;
+        private JTextArea display;
         private int lastFrame;
-        private int ct;
+        private int from, to, ct;
         
         public TestPane() {
             setLayout(new GridBagLayout());
@@ -75,8 +79,21 @@ public class Music {
             play = new JButton(">");
             stop = new JButton("[ ]");
             loop = new JButton("O");
+            startHere = new JButton("->");
+            endHere = new JButton("<-");
+            loopIt = new JButton("O!");
+            reset = new JButton("R");
+            display = new JTextArea();
+            display.setEditable(false);
             
             JPanel controls = new JPanel();
+            JPanel controlsLoop = new JPanel();
+            JPanel tField = new JPanel();
+            tField.add(display);
+            controlsLoop.add(startHere);
+            controlsLoop.add(endHere);
+            controlsLoop.add(loopIt);
+            controlsLoop.add(reset);
             controls.add(play);
             controls.add(stop);
             controls.add(loop);
@@ -89,6 +106,55 @@ public class Music {
             add(audioFile, gbc);
             gbc.gridy++;
             add(controls, gbc);
+            gbc.gridy++;
+            add(controlsLoop, gbc);
+            gbc.gridy++;
+            add(tField, gbc);
+            startHere.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (clip != null) {
+                        from = clip.getFramePosition();
+                        display.setText("Playing loop from position " + from);
+                    }
+                }
+            });
+            endHere.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (clip != null) {
+                        to = clip.getFramePosition();
+                        display.setText("Playing loop till: " + to + "\nPress Loop it To start Loop!");
+                    }
+                }
+            });
+            loopIt.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (from != 0 && to != 0 && clip != null) {
+                        clip.setFramePosition(from);
+                        clip.setLoopPoints(from, to);
+                        clip.loop(Clip.LOOP_CONTINUOUSLY);
+                        display.setText("Mood Playing...");
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "You havent set the loop points or the song isn't playing!", "Error!", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            });
+            reset.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (clip != null) {
+                        clip.setFramePosition(clip.getFramePosition());
+                        clip.loop(0);
+                        display.setText("Mood Loop ended! Continuing playback from current position");
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "The song isn't playing!", "Error!", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            });
+            
+            
+            
+            
             
             
             loop.addActionListener(new ActionListener() {
@@ -97,18 +163,15 @@ public class Music {
                     System.out.println("ct:" + ct);
                     if (ct % 2 == 0) {
                         clip.loop(0);
-                        System.out.println("Entered this method");
+                        display.setText("Ending infinite Loop");
                     }
                     else {
-                        int here = clip.getFramePosition();
-                        clip.setLoopPoints(here, -1);
+                        clip.setLoopPoints(0, -1);
                         clip.loop(Clip.LOOP_CONTINUOUSLY);
-                        System.out.println("Entered here!");
+                        display.setText("Looping continuously");
                     }
                 }
             });
-            
-            
             play.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -116,6 +179,7 @@ public class Music {
                         try {
                             loadClip(new File(audioFile.getText()));
                             clip.start();
+                            display.setText("Playing...");
                             clip.addLineListener(new LineListener() {
                                 @Override
                                 public void update(LineEvent event) {
@@ -141,6 +205,7 @@ public class Music {
                             lastFrame = clip.getFramePosition();
                             System.out.println(lastFrame);
                             clip.stop();
+                            display.setText("Paused");
                         } else {
                             if (lastFrame < clip.getFrameLength()) {
                                 clip.setFramePosition(lastFrame);
@@ -148,12 +213,12 @@ public class Music {
                                 clip.setFramePosition(0);
                             }
                             clip.start();
+                            display.setText("Playing...");
                         }
                         
                     }
                 }
             });
-            
             
             stop.addActionListener(new ActionListener() {
                 @Override
@@ -163,13 +228,13 @@ public class Music {
                         lastFrame = 0;
                         clip.stop();
                         clip = null;
+                        display.setText("Playback Stopped");
                     }
                 }
             });
         }
         
         protected void loadClip(File audioFile) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
-            
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
             AudioFormat format = audioStream.getFormat();
             DataLine.Info info = new DataLine.Info(Clip.class, format);
